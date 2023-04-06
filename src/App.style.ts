a7cac25454615ff60411a8065f6485172b81a693
이찬호 <chanho@woowahan.com>
2023-04-06 23:44:48 +0900

    greeting 메세지 추가

---
+++ src/App.style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  height: 100vh;
+  // dark mode background
+  background-color: #1e1e1e;
+
+  // light mode background
+  // background-color: #f5f5f5;
+
+  // dark mode text
+  color: #f5f5f5;
+
+  // light mode text
+  // color: #1e1e1e;
+`
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Container } from 'App.style'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>반갑다</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Container>
+      <h1>반가워요</h1>
+      <br />
+      <h1>저는 이찬호입니다.</h1>
+    </Container>
   )
 }
 

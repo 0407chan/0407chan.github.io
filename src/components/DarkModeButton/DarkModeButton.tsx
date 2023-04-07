d47c62c51149fe827d1357f0e6d0e2e5436589b6
이찬호 <chanho@woowahan.com>
2023-04-07 18:16:27 +0900

    다크모드 버튼 추가

---
+++ src/components/DarkModeButton/DarkModeButton.tsx
@@ -0,0 +1,22 @@
+import { IMAGES } from 'constants/image'
+import { ModeType } from 'hooks/useDarkMode'
+import React from 'react'
+import * as S from './style'
+
+type Props = {
+  onThemeToggler: () => void
+  theme: ModeType
+}
+const DarkModeButton: React.FC<Props> = ({ onThemeToggler, theme }) => {
+  return (
+    <S.Container onClick={onThemeToggler}>
+      <img
+        alt="theme-button"
+        draggable={false}
+        src={theme === 'DARK' ? IMAGES.SUN_FILL : IMAGES.MOON_FILL}
+      />
+    </S.Container>
+  )
+}
+
+export default DarkModeButton

c13b4319f22fa4f3daffd6fb1a6a5bfb84311770
이찬호 <chanho@woowahan.com>
2023-04-13 22:34:14 +0900

    테스트 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts'
+    // * NOTE 테스트 속도 저하 문제로 CSS를 테스트에 사용할 경우에만 true로 해준다. 현재는 필요없어 비활성화 처리함.
+    // css: true,
+  },
   plugins: [react(), checker({ typescript: true }), tsconfigPaths()],
   server: {
     port: 3000

8762e1b088c8a1858ff2f48555f09b79429a1f12
이찬호 <chanho@woowahan.com>
2025-01-29 00:42:52 +0900

    feat: App 기본 레이아웃잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { Container } from 'App.style'
-import DarkModeButton from 'components/DarkModeButton'
-import Vertical from 'components/Vertical/Vertical'
+import Appbar from 'components/AppBar/Appbar'
+import Dock from 'components/Dock/Dock'
+import Vertical from 'components/Vertical'
+import { IMAGES } from 'constants/image'
 import { darkTheme, lightTheme } from 'constants/themes'
 import { useDarkMode } from 'hooks/useDarkMode'
 import { ThemeProvider } from 'styled-components'
+import styles from './App.module.scss'
 
 export default function App() {
   const { theme, themeToggler } = useDarkMode()
 
   return (
     <ThemeProvider theme={theme === 'LIGHT' ? lightTheme : darkTheme}>
-      <Container>
-        <Vertical style={{ alignItems: 'center' }}>
-          <h1>반가워요</h1>
-          <h1>저는 이찬호입니다.</h1>
-          <DarkModeButton theme={theme} onThemeToggler={themeToggler} />
-        </Vertical>
-      </Container>
+      <div
+        className={styles.app}
+        style={{ backgroundImage: `url(${IMAGES.BACKGROUND.DEAD_SEA})` }}
+      >
+        <Appbar />
+        <Vertical className={styles.content}> </Vertical>
+        <Dock />
+      </div>
     </ThemeProvider>
   )
 }

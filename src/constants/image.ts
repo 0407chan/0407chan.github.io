f9ac6463d3f9daa9cb89b38c7c39be01d1ed1c82
이찬호 <chanho@woowahan.com>
2025-01-29 00:43:07 +0900

    feat: image constants 추가

--- src/constants/image.ts
+++ src/constants/image.ts
@@ -1,5 +1,15 @@
 export const IMAGES = {
   // DarkMode
   MOON_FILL: `./images/Moon_fill.png`,
-  SUN_FILL: `./images/Sun_fill.png`
+  SUN_FILL: `./images/Sun_fill.png`,
+
+  SERVICE: {
+    MONOTASK: './images/services/monotask.png',
+    RESPRING: './images/services/respring.png',
+    ZEMO: './images/services/zemo.png'
+  },
+
+  BACKGROUND: {
+    DEAD_SEA: './images/background/dead-sea.jpeg'
+  }
 }

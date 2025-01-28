1a6f542ed6a8c7c9f7cacf909dc167a30f29768e
이찬호 <chanho@woowahan.com>
2025-01-29 00:42:20 +0900

    feat: Vertical, Horizontal 컴포넌트 최신화

--- src/components/Horizontal/index.tsx
+++ src/components/Horizontal/index.tsx
@@ -1,1 +1,41 @@
-export { default } from './Horizontal'
+import clsx from "clsx";
+import styles from "./style.module.scss";
+
+export type HorizontalProps = {
+  gap?: string | number;
+  _ref?: React.RefObject<HTMLDivElement>;
+  style?: React.CSSProperties;
+  className?: string;
+  children: React.ReactNode;
+  id?: string;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
+  onScroll?: (e: React.UIEvent<HTMLDivElement, UIEvent>) => void;
+};
+
+const Horizontal = ({
+  gap,
+  _ref,
+  style,
+  className,
+  children,
+  id,
+  onMouseEnter,
+  onMouseLeave,
+  onScroll,
+}: HorizontalProps) => {
+  return (
+    <div
+      ref={_ref}
+      id={id}
+      className={clsx(styles.horizontal, className)}
+      style={{ gap, ...style }}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onScroll={onScroll}>
+      {children}
+    </div>
+  );
+};
+
+export default Horizontal;
--- src/components/Vertical/index.tsx
+++ src/components/Vertical/index.tsx
@@ -1,1 +1,22 @@
-export { default } from './Vertical'
+import clsx from "clsx";
+import { HorizontalProps } from "../Horizontal";
+import styles from "./style.module.scss";
+
+const Vertical = ({
+  _ref,
+  gap,
+  style,
+  className,
+  children,
+}: HorizontalProps) => {
+  return (
+    <div
+      ref={_ref}
+      className={clsx(styles.vertical, className)}
+      style={{ gap, ...style }}>
+      {children}
+    </div>
+  );
+};
+
+export default Vertical;

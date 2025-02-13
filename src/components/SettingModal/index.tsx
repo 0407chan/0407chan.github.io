9eea5b636aeda8fdd95df18fc868c81c8568c540
이찬호 <chanho@woowahan.com>
2025-02-14 01:20:37 +0900

    feat: 모달사용법 알았다.
    - 좀더 수정이 필요할것같다.
    - 쓰는쪽에서 커스텀을 어느정도까지 할수있을지, 어디까지 안해도될지 감 좀더 잡아보자

--- src/components/SettingModal/index.tsx
+++ src/components/SettingModal/index.tsx
@@ -1,5 +1,19 @@
-import Modal from 'components/Modal'
+import Vertical from 'components/Vertical'
 
 export default function SettingModal() {
-  return <Modal modalId="setting">SettingModal</Modal>
+  return (
+    <Vertical>
+      안녕하세요?
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+      <Vertical>반가워요</Vertical>
+    </Vertical>
+  )
 }

dee692712ddae13a7432da52a7eb1c0e5afba981
이찬호 <chanho@woowahan.com>
2025-02-15 00:02:08 +0900

    feat: 도크 아이콘에 툴팁 추가 및 스타일 개선

--- src/components/Dock/Dock.tsx
+++ src/components/Dock/Dock.tsx
@@ -1,18 +1,39 @@
+import { DefaultMantineColor, Tooltip, TransitionProps } from '@mantine/core'
 import { IMAGES } from 'constants/image'
 import styles from './Dock.module.scss'
 
+const COMMON_TOOLTIP_PROPS = {
+  color: 'rgba(246,246,246,0.9)' as DefaultMantineColor,
+  withArrow: true as boolean,
+  transitionProps: {
+    transition: 'fade',
+    duration: 0
+  } as Partial<Omit<TransitionProps, 'mounted'>>,
+  styles: {
+    tooltip: {
+      color: '#000',
+      backgroundColor: 'rgba(246,246,246,0.9)'
+    }
+  }
+}
 const Dock = () => {
   return (
     <div className={styles.dock}>
-      <div className={styles.icon}>
-        <img src={IMAGES.SERVICE.MONOTASK} alt="monotask" />
-      </div>
-      <div className={styles.icon}>
-        <img src={IMAGES.SERVICE.RESPRING} alt="respring" />
-      </div>
-      <div className={styles.icon}>
-        <img src={IMAGES.SERVICE.ZEMO} alt="zemo" />
-      </div>
+      <Tooltip label="Monotask" {...COMMON_TOOLTIP_PROPS}>
+        <div className={styles.icon}>
+          <img src={IMAGES.SERVICE.MONOTASK} alt="monotask" />
+        </div>
+      </Tooltip>
+      <Tooltip label="Respring" {...COMMON_TOOLTIP_PROPS}>
+        <div className={styles.icon}>
+          <img src={IMAGES.SERVICE.RESPRING} alt="respring" />
+        </div>
+      </Tooltip>
+      <Tooltip label="Zemo" {...COMMON_TOOLTIP_PROPS}>
+        <div className={styles.icon}>
+          <img src={IMAGES.SERVICE.ZEMO} alt="zemo" />
+        </div>
+      </Tooltip>
     </div>
   )
 }

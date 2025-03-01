b5b1b635f0b061b0892d4e0671403d2df7054ef0
이찬호 <chanho@woowahan.com>
2025-03-01 23:48:53 +0900

    feat: 프로젝트 타입에 카테고리 추가
    📝 Details:
    - Project 타입에 category 필드 추가
    - ModalCategory 타입 임포트

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,9 @@
+import { ModalCategory } from 'contexts/modal/modalDefinitions'
+
 export type Project = {
   id: string
   name: string
   description?: string
   image?: string
+  category: ModalCategory
 }
